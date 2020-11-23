603580b373c54271385ddc6868816f81a88bc0f3
Alex Subota <as.subota@gmail.com>
2020-11-24 00:22:59 +0200

    Implement custom color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'CatsApp',
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          ),
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primaryColor: Color(0xfff48fb1),
+        primaryColorDark: Color(0xffbf5f82),
+        primaryColorLight: Color(0xffffc1e3),
+        accentColor: Color(0xfff06292),
+      ),
       home: MyHomePage(title: 'CatsApp'),
     );
   }
